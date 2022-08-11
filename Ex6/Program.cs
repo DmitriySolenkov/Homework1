d84e2040e9e5ce2a2b268af0977b7c60f6140a1d
DmitriySolenkov <soledimk@gmail.com>
2022-08-11 16:16:56 +0300

    Ex6

---
+++ Ex6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num % 2;
+if (num2 == 0)
+{
+    Console.Write("Number is even!");
+}
+else
+{
+    Console.Write("Number is not even!");
+}
