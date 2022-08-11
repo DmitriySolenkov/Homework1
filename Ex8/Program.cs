33957ed2b216a781d54e605b1d699b1e5f3580a2
DmitriySolenkov <soledimk@gmail.com>
2022-08-11 16:21:35 +0300

    Ex8

---
+++ Ex8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= num)
+{
+    Console.Write(count+", ");
+    count = count + 2;
+}
