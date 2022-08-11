5084c540e5987aa96ab7fa0bae6bf3e22ebb0449
DmitriySolenkov <soledimk@gmail.com>
2022-08-11 16:07:03 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine("Max = " + num1 + ", min = "+ num2);
+}
+if (num1<num2)
+{
+    Console.WriteLine("Max = " + num2 + ", min = "+ num1);
+}
+if (num1==num2)
+{
+    Console.WriteLine("Numbers are equal");
+}
