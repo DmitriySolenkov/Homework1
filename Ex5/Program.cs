fcad1d0fb8a5ee50dddc2aa5de39696358b7c30d
DmitriySolenkov <soledimk@gmail.com>
2022-08-11 16:30:26 +0300

    Ex5

---
+++ Ex5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = -num;
+while (num2<=num)
+{
+    Console.Write(num2+ ", ");
+    num2++;
+}
